c6c94780594003be15cffa89b31ce87f438588e9
Joseph Obiagba <j.i.obiagba2016@gmail.com>
2020-01-05 16:36:01 +0100

    Test setup cpmplete for CI/CD

--- main/init.ts
+++ main/init.ts
@@ -4,6 +4,15 @@ import { App } from "./app"
 const app = new App().app,
         PORT: number | string = process.env.PORT || 1010,
         server = http.createServer(app)
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}!`)
-})
+
+const power = ():void => {
+    server.listen(PORT, ():void => {
+        console.log(`Server running on port ${PORT}!`)
+    })
+}
+
+if(require.main === module) {
+    power()
+} else {
+    exports.starter = power
+}
